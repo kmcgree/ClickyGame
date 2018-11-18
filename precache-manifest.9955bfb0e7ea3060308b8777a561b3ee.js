b758486779f78ca4bf8f51feb5962b321d7ae743
Kane McGree <kmcgree14@gmail.com>
2018-11-17 19:47:12 -0600

    Updates

---
+++ precache-manifest.9955bfb0e7ea3060308b8777a561b3ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "6e5846b99204e9cef85c",
+    "url": "/ClickyGame/static/js/main.6e5846b9.chunk.js"
+  },
+  {
+    "revision": "8bf809dc9a4e8b20b058",
+    "url": "/ClickyGame/static/js/1.8bf809dc.chunk.js"
+  },
+  {
+    "revision": "6e5846b99204e9cef85c",
+    "url": "/ClickyGame/static/css/main.4a039018.chunk.css"
+  },
+  {
+    "revision": "af3d88643dfff49e979e57b4d16616fc",
+    "url": "/ClickyGame/index.html"
+  }
+];
